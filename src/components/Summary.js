bb53420f7507ef62065a7b05b62ca7758f710094
Vignesh Vijayakumar <vignesh.vijayakumar@nagarro.com>
2024-07-02 18:03:27 +0530

    adding summary logic

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,8 +1,37 @@
+import { Box, Typography } from "@mui/material"
+import { useEffect,useState } from "react"
+import useFinance from "../hooks/useFinance"
+
 const Summary = () => {
+    const {financialData,getFinancialData} = useFinance()
+    const [income,setIncome] = useState(0)
+    const [expense,setExpense] = useState(0)
+    
+    useEffect(()=>{
+        getFinancialData()
+    },[])
+
+    useEffect(()=>{
+        let totalIncome = 0
+        let totalExpense = 0
+        financialData?.map((data)=>{
+            if(data.type === "income"){
+                totalIncome = totalIncome + parseInt(data?.amount)
+            } else {
+                totalExpense = totalExpense + parseInt(data?.amount)
+            }
+        })
+        setIncome(totalIncome)
+        setExpense(totalExpense)
+    },[financialData])
+   
     return (
-        <div>
-            <h1>Summary</h1>
-        </div>
+        <Box>
+            <Typography variant="h4">Summary</Typography>
+           {income !== 0 && <Typography variant="h4">Total Income {income}</Typography>}
+           {expense !== 0 && <Typography variant="h4">Total Expense {expense} </Typography>}
+           { income !== 0  && expense !== 0 && <Typography variant="h4">Balance {income-expense} </Typography> }
+        </Box>
     )
 }
 
--- src/hooks/useFinance.js
+++ src/hooks/useFinance.js
@@ -4,23 +4,8 @@ import { getHttpClient } from "../api/axiosClient";
 
 const useFinance = () => {
     const[loading,setLoading] = useState(false)
-    const[expense,setExpense] = useState([])
-    const[income,setIncome] = useState([])
     const[financialData,setFinancialData] = useState([])
 
-    const getExpenses = useCallback(() => {
-       const filteredExpense =  financialData?.filter((data)=>data?.type === 'expense')
-       if(filteredExpense?.length >= 1){
-        setExpense(filteredExpense)
-       }
-    },[])
-
-    const getIncomes = useCallback(()=>{
-        const filteredIncome = financialData?.filter((data)=>data?.type === 'income')
-        if(filteredIncome.length >= 1){
-            setIncome(filteredIncome)
-        }
-    },[])
 
     const getFinancialData = useCallback(async()=>{
         try{
@@ -60,15 +45,11 @@ const useFinance = () => {
 
         return {
             loading,
-            income,
-            expense,
             financialData,
-            getExpenses,
-            getIncomes,
             getFinancialData,
             addNewFinancialData
         }
-    },[loading,expense,income,financialData,getExpenses,getIncomes,getFinancialData,addNewFinancialData])
+    },[loading,financialData,getFinancialData,addNewFinancialData])
 
     return values
 }
