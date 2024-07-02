7967bcbaca64cb7a8399d34361e5ecb80cf13fb7
Vignesh Vijayakumar <vignesh.vijayakumar@nagarro.com>
2024-07-02 17:39:44 +0530

    transactionList rendering table

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,26 @@
-import { Box, Typography } from "@mui/material"
-import { useEffect,useState } from "react"
-import useFinance from "../hooks/useFinance"
+import { Box, Typography } from "@mui/material";
+import { useEffect,useState } from "react";
+import useFinance from "../hooks/useFinance";
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+    { field: 'id', headerName: 'S.No', width: 100 },
+    { field: 'createdAt', headerName: 'Transaction Date', width: 150 },
+    { field: 'name', headerName: 'Transaction Name', width: 300 },
+    {
+      field: 'amount',
+      headerName: 'Amount',
+      width: 100,
+      renderCell: (row)=>{
+        return <span style={{color : row?.row?.type === 'income' ? "green" : "red"}}>{row?.row?.amount || 'No Amount'}</span>
+      }
+    },
+    {
+      field: 'type',
+      headerName: 'Type',
+      width: 160,
+    }
+  ];
 
 const TransactionList = () => {
     const {getFinancialData,financialData} = useFinance()
@@ -17,13 +37,20 @@ const TransactionList = () => {
     },[financialData])
 
     return(
-        <Box>
-            <Typography variant="h4">TransactionList</Typography>
-            {transactionLists?.length >= 1 && transactionLists?.map((data,index)=>{
-                return (<Box key={index}>
-                <p>{data?.name || 'No Name'}</p>
-                </Box>)
-            })}
+        <Box style={{width: '65%' }}>
+            <Typography variant="h4">Transaction List</Typography>
+
+            <DataGrid
+            rows={transactionLists}
+            columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10,25]}
+      />
+
         </Box>
     )
 }
